788aa34bec6b4a51bf7b8ca45bdcff6bed9c71ec
ShubhamMungal3002 <shubhammungal3002@gmail.com>
2025-11-05 17:40:32 +0530

    Comment out knapsack function and backtracking logic
    Commented out the knapsack function and related logic.

--- kanpsack _problem.py
+++ kanpsack _problem.py
@@ -30,25 +30,25 @@ if __name__ == "__main__":
     print(f"Maximum value in knapsack = {max_value}")
 
 
+# if wen have to print the selected items
+# def knapsack(weights, values, capacity):
+#    n = len(values)
+#     dp = [[0 for _ in range(capacity + 1)] for _ in range(n + 1)]
 
-#def knapsack(weights, values, capacity):
-   n = len(values)
-    dp = [[0 for _ in range(capacity + 1)] for _ in range(n + 1)]
+#     for i in range(1, n + 1):
+#           for w in range(1, capacity + 1):
+#             if weights[i - 1] <= w:
+#                 dp[i][w] = max(values[i - 1] + dp[i - 1][w - weights[i - 1]], dp[i - 1][w])
+#             else:
+#                 dp[i][w] = dp[i - 1][w]
 
-    for i in range(1, n + 1):
-          for w in range(1, capacity + 1):
-            if weights[i - 1] <= w:
-                dp[i][w] = max(values[i - 1] + dp[i - 1][w - weights[i - 1]], dp[i - 1][w])
-            else:
-                dp[i][w] = dp[i - 1][w]
+#     # Backtrack to find which items were included
+#     selected = []
+#     w = capacity
+#     for i in range(n, 0, -1):
+#         if dp[i][w] != dp[i - 1][w]:
+#             selected.append(i)
+#             w -= weights[i - 1]
 
-    # Backtrack to find which items were included
-    selected = []
-    w = capacity
-    for i in range(n, 0, -1):
-        if dp[i][w] != dp[i - 1][w]:
-            selected.append(i)
-            w -= weights[i - 1]
-
-    print("Selected item indices:", selected[::-1])
-    return dp[n][capacity]
+#     print("Selected item indices:", selected[::-1])
+#     return dp[n][capacity]
